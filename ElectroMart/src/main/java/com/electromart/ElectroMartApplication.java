feae4e79a48642e83acc05d5579fd722a28efb2f
Huseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
2025-06-16 14:06:38 +0200

    update

--- ElectroMart/src/main/java/com/electromart/ElectroMartApplication.java
+++ ElectroMart/src/main/java/com/electromart/ElectroMartApplication.java
@@ -1,31 +1,13 @@
 package com.electromart;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ElectroMartApplication {
 
 	public static void main(String[] args) {
-
-		SessionFactory factory = new Configuration()//
-				.configure()//
-				.addAnnotatedClass(Actors.class)//
-				.buildSessionFactory();
-		try (Session session = factory.openSession()) {
-			var transaction = session.beginTransaction();
-			var actor = session.get(Actors.class, 201);
-			System.out.println(actor.getFirstName());
-			actor.setFirstName("Ben");
-			session.merge(actor);
-			// for (var actor : actors) {
-			// System.out.println(actor.getId() + ": " + actor.getFirstName() + " " +
-			// actor.getLastName());
-			// }
-			transaction.commit();
-		} finally {
-			factory.close();
-		}
+		SpringApplication.run(ElectroMartApplication.class, args);
 	}
 
 }
