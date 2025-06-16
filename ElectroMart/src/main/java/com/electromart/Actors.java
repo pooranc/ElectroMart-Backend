a4b701d709e72e291c035177045a574cdbab6c3f
Huseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
2025-06-16 13:58:31 +0200

    hibernate integration and crud

---
+++ ElectroMart/src/main/java/com/electromart/Actors.java
@@ -0,0 +1,62 @@
+package com.electromart;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "actor")
+@Table(name = "actor")
+public class Actors {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "actor_id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "last_update")
+	private LocalDateTime lastUpdate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+}
--- ElectroMart/src/main/java/com/electromart/ElectroMartApplication.java
+++ ElectroMart/src/main/java/com/electromart/ElectroMartApplication.java
@@ -1,13 +1,31 @@
 package com.electromart;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-@SpringBootApplication
 public class ElectroMartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElectroMartApplication.class, args);
+
+		SessionFactory factory = new Configuration()//
+				.configure()//
+				.addAnnotatedClass(Actors.class)//
+				.buildSessionFactory();
+		try (Session session = factory.openSession()) {
+			var transaction = session.beginTransaction();
+			var actor = session.get(Actors.class, 201);
+			System.out.println(actor.getFirstName());
+			actor.setFirstName("Ben");
+			session.merge(actor);
+			// for (var actor : actors) {
+			// System.out.println(actor.getId() + ": " + actor.getFirstName() + " " +
+			// actor.getLastName());
+			// }
+			transaction.commit();
+		} finally {
+			factory.close();
+		}
 	}
 
 }
